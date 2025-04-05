d5ea74d838288f23fb72c81597bd62377660f937
Bhargav0508 <bhargav@tryspeed.com>
2025-04-05 14:42:18 +0530

    feat: add support for single and two comma-separated numbers

--- calculator.js
+++ calculator.js
@@ -2,6 +2,9 @@ function add(input) {
   if (input === "") {
     return 0; // Return 0 for an empty string
   }
+
+  const numbers = input.split(",").map(Number);
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
