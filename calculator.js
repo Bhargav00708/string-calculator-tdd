2ffdf72e364a1f62cbed0faeaff2fea483c2c6ee
Bhargav0508 <bhargav@tryspeed.com>
2025-04-05 14:54:13 +0530

    feat: support newline delimiters along with commas

--- calculator.js
+++ calculator.js
@@ -3,7 +3,10 @@ function add(input) {
     return 0; // Return 0 for an empty string
   }
 
-  const numbers = input.split(",").map(Number);
+  const numbers = input
+    .split(/[\n,]/) // Regex to split on both comma and newline
+    .map(Number);
+
   return numbers.reduce((sum, num) => sum + num, 0);
 }
 
