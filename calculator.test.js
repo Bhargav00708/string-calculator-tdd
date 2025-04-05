02eb54eb53d13a740817ab1ababa04eaef858536
Bhargav0508 <bhargav@tryspeed.com>
2025-04-05 14:53:10 +0530

    test: handle newlines as delimiters

--- calculator.test.js
+++ calculator.test.js
@@ -16,3 +16,7 @@ test("should return sum when multiple numbers are passed", () => {
   expect(add("1,2,3")).toBe(6);
   expect(add("1,2,3,4,5")).toBe(15);
 });
+
+test("should handle newlines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
