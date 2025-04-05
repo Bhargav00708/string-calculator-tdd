c0d872dece2ff2250a25e4fb099119448ba1149a
Bhargav0508 <bhargav@tryspeed.com>
2025-04-05 14:41:58 +0530

    test: handle single and two comma-separated numbers

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,11 @@ const { add } = require("./calculator");
 test("should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return number when only one number is passed", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("should return sum when two numbers are passed", () => {
+  expect(add("1,5")).toBe(6);
+});
